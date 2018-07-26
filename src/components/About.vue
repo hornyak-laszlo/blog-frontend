d654750984b87634d2e3f6116b26bb53058b659a
laco <hornyak.laszlo88@gmail.com>
2018-07-26 18:15:03 +0200

    About page updated

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="title">About me</h1>
-    <h2 class="subtitle">About me</h2>
+    <h2 class="subtitle">Some information about me</h2>
     <b-collapse class="card">
       <div slot="trigger" slot-scope="props" class="card-header">
           <p class="card-header-title">
@@ -108,7 +108,7 @@ biggest projects were a social media site, a vehicle tracking site, a B2B site a
           <div class="level">
             <div class="level-left">
               <div class="level-item">
-                <p class="is-italic">Compronet GmbH., Fürth</p>
+                <p class="is-italic">Compronet GmbH., Fürth (DE)</p>
               </div>
             </div>
             <div class="level-right">
@@ -122,6 +122,118 @@ For development we used NodeJs, Angular stack with MongoDB and/or external REST 
 Our project management toolset was from Atlassian (Jira, Confluence, Bitbucket). Some of my
 biggest projects were a working clothes administration site, a weather statistics analyzer site and a car
 tracking statistical site.</p>
+        </div>
+      </div>
+    </b-collapse>
+    <br>
+    <b-collapse class="card">
+      <div slot="trigger" slot-scope="props" class="card-header">
+          <p class="card-header-title">
+            PROJECTS
+          </p>
+          <a class="card-header-icon">
+            <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
+          </a>
+      </div>
+      <div class="card-content">
+        <div class="content">
+          <p class="has-text-weight-bold">Working clothes administration site</p>
+          <ul>
+            <li><i>Backend:</i> ExpressJS (NodeJS) - MongoDB - IBM AS400</li>
+            <li><i>Frontend:</i> AngularJS - Bootstrap 3</li>
+            <li>
+              We used the MEAN Stack for this project. We got the data from a remote service, provided by an
+              IBM AS400 server. We used a MongoDB database mostly for caching. Our backend was
+              developed as a REST API. Our frontend was created in AngularJS. We used Bootstrap to make
+              the design nice and responsive.
+            </li>
+            <li>
+              This was a really complex site containing several administration pages and statistical modules.
+              The administration pages were ticket based. For every interaction the user maintained a ticket
+              (size changed, repair needed, new clothes needed ...). On the statistical pages we showed for
+              example the number of outgoing and incoming clothes on chart and table as well.
+            </li>
+          </ul>
+          <p class="has-text-weight-bold">Car tracking site</p>
+          <ul>
+            <li><i>Backend:</i> Remote REST API</li>
+            <li><i>Frontend:</i> AngularJS 1.5 (component based, ES6) - Bootstrap 3</li>
+            <li>
+              This was an only frontend project. We got the data from a REST API provided by another
+              company. We used the newest stable AngularJS 1.5 with the new ES6 syntax. We used
+              component based architecture, with that the maintainability of our codebase dramatically
+              increased. For the building process we used Webpack.
+            </li>
+            <li>
+              This site was about tracking the route of the cars, showing and administrating the sessions of the
+              routes. We showed the routes of the cars on a map, and provided administration pages for the
+              sessions and cars. We had as well statistical diagrams about the runs, containing the speed,
+              consumption and other data.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </b-collapse>
+    <br>
+    <b-collapse class="card">
+      <div slot="trigger" slot-scope="props" class="card-header">
+          <p class="card-header-title">
+            SKILLS
+          </p>
+          <a class="card-header-icon">
+            <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
+          </a>
+      </div>
+      <div class="card-content">
+        <div class="content">
+          <p class="has-text-weight-bold">Languages</p>
+          <div class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <p class="is-italic">Hungarian</p>
+              </div>
+            </div>
+            <div class="level-right">
+              <div class="level-item">
+                <span class="is-italic">Native</span>
+              </div>
+            </div>
+          </div>
+          <div class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <span class="is-italic">English</span>&nbsp;
+                <span> - English intermediate level exam (2011)</span>
+              </div>
+            </div>
+            <div class="level-right">
+              <div class="level-item">
+                <span class="is-italic">B2 Level</span>
+              </div>
+            </div>
+          </div>
+          <div class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <p class="is-italic">German</p>
+              </div>
+            </div>
+            <div class="level-right">
+              <div class="level-item">
+                <span class="is-italic">B2 Level</span>
+              </div>
+            </div>
+          </div>
+          <p class="has-text-weight-bold">Programming languages and frameworks</p>
+          <p class="is-italic">Prior experience</p>
+          <p>Android, C, C++, Java EE, Matlab, Python, R, Scala</p>
+          <p class="is-italic">Proficient</p>
+          <p>CodeIgniter, Java, Laravel, React, Vue</p>
+          <p class="is-italic">Expert</p>
+          <p>Angular, Bootstrap, CSS, ExpressJS, HTML, JavaScript, MongoDB, MySQL, PHP</p>
+          <p class="has-text-weight-bold">Soft skills and qualities</p>
+          <p>Good problem solving, quick of understanding, creative thinking.</p>
+          <p>Openness and desire to learn and develop.</p>
         </div>
       </div>
     </b-collapse>
