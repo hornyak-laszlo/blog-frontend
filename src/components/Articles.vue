a84a91e0fbdd9c52662f5e72fbeb0ac3582d884a
laco <hornyak.laszlo88@gmail.com>
2018-08-01 01:27:51 +0200

    Articles and courses

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,22 +1,81 @@
 <template>
   <div>
     <h1 class="title">Articles</h1>
-    <h2 class="subtitle">I am listing here the articles which I really useful found</h2>
-    <div class="card">
-      <div class="card-header">
-        <p class="card-header-title">
-          Header
-        </p>
-      </div>
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+    <h2 class="subtitle">I am listing here the articles which I found really useful</h2>
+    <b-tabs type="is-toggle is-fullwidth">
+      <b-tab-item>
+        <template slot="header">
+          <b-icon icon="language-javascript"></b-icon>
+          <span> Javascript <b-tag rounded> 15 </b-tag> </span>
+        </template>
+        <div class="box">
+          <ul>
+            <li><a target="_blank" href="https://codeburst.io/things-you-need-to-know-to-become-an-ace-javascript-developer-aa703397268d">Things you need to know to become an ace Javascript developer</a></li>
+            <li><a target="_blank" href="https://medium.com/javascript-scene/functional-mixins-composing-software-ffb66d5e731c">Functional Mixins</a></li>
+            <li><a target="_blank" href="https://blog.risingstack.com/javascript-clean-coding-best-practices-node-js-at-scale/">JavaScript Clean Coding Best Practices</a></li>
+            <li><a target="_blank" href="https://blog.risingstack.com/node-js-development-tips-2018/">8 Tips to Build Better Node.js Apps in 2018</a></li>
+            <li><a target="_blank" href="https://blog.risingstack.com/node-js-security-checklist/">Node.js Security Checklist</a></li>
+            <li><a target="_blank" href="https://blog.risingstack.com/mastering-async-await-in-nodejs/">Mastering Async Await in Node.js</a></li>
+            <li><a target="_blank" href="https://blog.risingstack.com/10-best-practices-for-writing-node-js-rest-apis/">10 Best Practices for Writing Node.js REST APIs</a></li>
+            <li><a target="_blank" href="https://medium.com/javascript-scene/composing-software-an-introduction-27b72500d6ea">Composing Software: An Introduction</a></li>
+            <li><a target="_blank" href="https://medium.com/javascript-scene/the-rise-and-fall-and-rise-of-functional-programming-composable-software-c2d91b424c8c">The Rise and Fall and Rise of Functional Programming</a></li>
+            <li><a target="_blank" href="https://medium.com/javascript-scene/why-learn-functional-programming-in-javascript-composing-software-ea13afc7a257">Why Learn Functional Programming in JavaScript? </a></li>
+            <li><a target="_blank" href="https://scotch.io/tutorials/a-practical-graphql-getting-started-guide-with-nodejs">A Practical GraphQL Getting Started Guide with Node.js</a></li>
+            <li><a target="_blank" href="https://scotch.io/courses/build-an-online-shop-with-vue">Build an Online Shop with Vue</a></li>
+            <li><a target="_blank" href="https://gist.github.com/staltz/868e7e9bc2a7b8c1f754">The introduction to Reactive Programming you've been missing</a></li>
+            <li><a target="_blank" href="https://codeburst.io/angular-vs-react-vs-vue-f470f5b74bf6">Angular vs React vs Vue</a></li>
+            <li><a target="_blank" href="https://blog.bitsrc.io/what-is-this-in-javascript-3b03480514a7">What is “this” in JavaScript?</a></li>
+          </ul>
         </div>
-      </div>
-      <footer class="card-footer">
-          <a class="card-footer-item has-text-info">Continue reading</a>
-      </footer>
-    </div>
+      </b-tab-item>
+      <b-tab-item>
+        <template slot="header">
+          <b-icon icon="lambda"></b-icon>
+          <span> Functional Programming <b-tag rounded> 14 </b-tag> </span>
+        </template>
+        <div class="box">
+          <ul>
+            <li><a target="_blank" href="http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html">Functors, Applicatives, And Monads In Pictures</a></li>
+            <li><a target="_blank" href="https://jrsinclair.com/articles/2016/marvellously-mysterious-javascript-maybe-monad/">The Marvellously Mysterious Javascript Maybe Monad</a></li>
+            <li><a target="_blank" href="https://tech.iheart.com/why-fp-its-the-composition-f585d17b01d3">Why Functional Programming? It’s the composition.</a></li>
+            <li><a target="_blank" href="https://functional.works-hub.com/learn/Functional-Programming-Jargon">Functional Programming Jargon</a></li>
+            <li><a target="_blank" href="https://hkupty.github.io/2016/Functional-Programming-Concepts-Idioms-and-Philosophy/">Functional Programming: Concepts, Idioms and Philosophy</a></li>
+            <li><a target="_blank" href="https://functional.works-hub.com/learn/Understanding-the-Math-behind-FP-The-Monads">Understanding the Math behind FP: The Monads</a></li>
+            <li><a target="_blank" href="https://medium.com/javascript-scene/a-functional-programmers-introduction-to-javascript-composing-software-d670d14ede30">A Functional Programmer’s Introduction to JavaScript</a></li>
+            <li><a target="_blank" href="https://medium.com/javascript-scene/javascript-monads-made-simple-7856be57bfe8">Javascript Monads Made Simple</a></li>
+            <li><a target="_blank" href="https://medium.com/javascript-scene/higher-order-functions-composing-software-5365cf2cbe99">Higher Order Functions</a></li>
+            <li><a target="_blank" href="https://medium.freecodecamp.org/functional-programming-in-js-with-practical-examples-part-1-87c2b0dbc276">Functional Programming In JavaScript With Practical Examples (Part 1)</a></li>
+            <li><a target="_blank" href="https://medium.freecodecamp.org/functional-programming-in-js-with-practical-examples-part-2-429d2e8ccc9e">Functional Programming In JavaScript With Practical Examples (Part 2)</a></li>
+            <li><a target="_blank" href="https://functional.works-hub.com/learn/the-science-behind-functional-programming-3b060?utm_campaign=Newsletter&utm_source=hs_email&utm_medium=email&utm_content=62090173&_hsenc=p2ANqtz-_cm_1lgULsvbDe_dqKyXG_2v_0cSnsn7TtW31DapXRr5kwRcS3ljOavNVtCSIyAQIu69MCi5MPZxxOuEnq_0gTr65VaB7af447sfz40sJ-C8aQjQE&_hsmi=62090173">The Science Behind Functional Programming</a></li>
+            <li><a target="_blank" href="https://functional.works-hub.com/learn/demystifying-the-monad-in-scala-cf414?utm_campaign=Newsletter&utm_source=hs_email&utm_medium=email&utm_content=61594389&_hsenc=p2ANqtz-8zg8gyhbu7YMq9mFZjTP0NDdvFdUrF7gZ3UmAS2FxmZ5FCB3H4zTOlEtvY5DgiCx4qPO7tQaRHpAYq2TOLOGGR-xIsHWrS6wcSeEHrXCSdJREjr5A&_hsmi=61594389">Demystifying the Monad in Scala</a></li>
+            <li><a target="_blank" href="https://snipcart.com/blog/functional-programming-paradigm-concepts">Concepts behind the Functional Programming Paradigm Explained</a></li>
+          </ul>
+        </div>
+      </b-tab-item>
+      <b-tab-item>
+        <template slot="header">
+            <b-icon icon="chart-bar"></b-icon>
+            <span> Data Science / Machine Learning <b-tag rounded> 13 </b-tag> </span>
+        </template>
+        <div class="box">
+          <ul>
+            <li><a target="_blank" href="https://towardsdatascience.com/top-algorithms-and-data-structures-you-really-need-to-know-ab9a2a91c7b5">Top Algorithms and Data Structures You Really Need To Know</a></li>
+            <li><a target="_blank" href="https://medium.com/machine-learning-for-humans/why-machine-learning-matters-6164faf1df12">Machine Learning for Humans</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/a-beginners-guide-to-the-data-science-pipeline-a4904b2d8ad3">A Beginner’s Guide to the Data Science Pipeline</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/a-tour-of-the-top-10-algorithms-for-machine-learning-newbies-dde4edffae11">A Tour of The Top 10 Algorithms for Machine Learning Newbies</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/introduction-to-machine-learning-db7c668822c4">Introduction To Machine Learning</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/data-science-interview-guide-4ee9f5dc778">Data Science Interview Guide</a></li>
+            <li><a target="_blank" href="https://medium.com/@srowen/common-probability-distributions-347e6b945ce4">Common Probability Distributions</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/introduction-to-statistics-e9d72d818745">Data science concepts you need to know!</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/data-science-you-need-to-know-a-b-testing-f2f12aff619a">A/B testing</a></li>
+            <li><a target="_blank" href="https://medium.com/cracking-the-data-science-interview/the-10-statistical-techniques-data-scientists-need-to-master-1ef6dbd531f7">The 10 Statistical Techniques Data Scientists Need to Master</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/a-complete-machine-learning-walk-through-in-python-part-one-c62152f39420">A Complete Machine Learning Project Walk-Through in Python: Part One</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/a-complete-machine-learning-project-walk-through-in-python-part-two-300f1f8147e2">A Complete Machine Learning Project Walk-Through in Python: Part Two</a></li>
+            <li><a target="_blank" href="https://towardsdatascience.com/a-complete-machine-learning-walk-through-in-python-part-three-388834e8804b">A Complete Machine Learning Project Walk-Through in Python: Part Three</a></li>
+          </ul>
+        </div>
+      </b-tab-item>
+    </b-tabs>
   </div>
 </template>
 
--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -2,6 +2,170 @@
   <div>
     <h1 class="title">Courses</h1>
     <h2 class="subtitle">I am listing here the courses which I participated in</h2>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Algorithms, Part I - Princeton University
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.coursera.org/learn/algorithms-part1/home/welcome">Coursera</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Algorithms, Part II - Princeton University
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.coursera.org/learn/algorithms-part2/home/welcome">Coursera</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Professor Frisby Introduces Composable Functional JavaScript
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript">Egghead</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Functional Programming Principles in Scala - École Polytechnique Fédérale de Lausanne
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.coursera.org/learn/progfun1/home/welcome">Coursera</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Functional Program Design in Scala - École Polytechnique Fédérale de Lausanne
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.coursera.org/learn/progfun2/home/welcome">Coursera</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Parallel programming - École Polytechnique Fédérale de Lausanne
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.coursera.org/learn/parprog1/home/welcome">Coursera</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Big Data Analysis with Scala and Spark - École Polytechnique Fédérale de Lausanne
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.coursera.org/learn/scala-spark-big-data/home/welcome">Coursera</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Machine Learning - Stanford University
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.coursera.org/learn/machine-learning/home/welcome">Coursera</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Data Science & Maschinelles Lernen in Python - am Beispiel
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.udemy.com/data-science-und-machine-learning/">Udemy</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Data Science A-Z™: Real-Life Data Science Exercises Included
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.udemy.com/datascience/">Udemy</a>
+          </span>
+        </p>
+      </footer>
+    </div>
+    <br>
+    <div class="card">
+      <div class="card-content">
+        <p class="has-text-centered has-text-weight-bold subtitle">
+          Neural Networks - 3Blue1Browm
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            Watch it on <a target="_blank" href="https://www.youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi" >Youtube</a>
+          </span>
+        </p>
+      </footer>
+    </div>
   </div>
 </template>
 
