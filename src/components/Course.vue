15d6b8179811035867f908bdcd25fa18a249ecce
Laszlo Hornyak <laszlo.hornyak@botfriends.de>
2019-05-27 18:40:04 +0200

    Courses updated

---
+++ src/components/Course.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <p class="has-text-centered has-text-weight-bold subtitle">
+        {{name}}
+      </p>
+    </div>
+    <footer class="card-footer">
+      <p class="card-footer-item">
+        <span>
+          Watch it on <a target="_blank" :href="url" >{{page}}</a>
+        </span>
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Course',
+  props: {
+    name: String,
+    url: String,
+    page: String
+  }
+}
+</script>
--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -2,176 +2,21 @@
   <div>
     <h1 class="title">Courses</h1>
     <h2 class="subtitle">I am listing here the courses which I participated in</h2>
-    <div class="card" v-for="course in courses" :key="course.id">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          {{course.name}}
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" :href="course.url">{{course.page}}</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Algorithms, Part II - Princeton University
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.coursera.org/learn/algorithms-part2/home/welcome">Coursera</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Professor Frisby Introduces Composable Functional JavaScript
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript">Egghead</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Functional Programming Principles in Scala - École Polytechnique Fédérale de Lausanne
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.coursera.org/learn/progfun1/home/welcome">Coursera</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Functional Program Design in Scala - École Polytechnique Fédérale de Lausanne
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.coursera.org/learn/progfun2/home/welcome">Coursera</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Parallel programming - École Polytechnique Fédérale de Lausanne
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.coursera.org/learn/parprog1/home/welcome">Coursera</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Big Data Analysis with Scala and Spark - École Polytechnique Fédérale de Lausanne
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.coursera.org/learn/scala-spark-big-data/home/welcome">Coursera</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Machine Learning - Stanford University
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.coursera.org/learn/machine-learning/home/welcome">Coursera</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Data Science & Maschinelles Lernen in Python - am Beispiel
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.udemy.com/data-science-und-machine-learning/">Udemy</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Data Science A-Z™: Real-Life Data Science Exercises Included
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.udemy.com/datascience/">Udemy</a>
-          </span>
-        </p>
-      </footer>
-    </div>
-    <br>
-    <div class="card">
-      <div class="card-content">
-        <p class="has-text-centered has-text-weight-bold subtitle">
-          Neural Networks - 3Blue1Browm
-        </p>
-      </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            Watch it on <a target="_blank" href="https://www.youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi" >Youtube</a>
-          </span>
-        </p>
-      </footer>
+    <div v-for="course in courses" :key="course.id">
+      <course :name="course.name" :page="course.page" :url="course.url"></course>
+      <br>
     </div>
   </div>
 </template>
 
 <script>
+import Course from './Course'
+
 export default {
   name: 'Courses',
+  components: {
+    Course
+  },
   data () {
     return {
       courses: [
@@ -180,14 +25,59 @@ export default {
           name: 'Algorithms, Part I - Princeton University',
           url: 'https://www.coursera.org/learn/algorithms-part1/home/welcome',
           page: 'Coursera'
+        }, {
+          id: 2,
+          name: 'Algorithms, Part II - Princeton University',
+          url: 'https://www.coursera.org/learn/algorithms-part2/home/welcome',
+          page: 'Coursera'
+        }, {
+          id: 3,
+          name: 'Professor Frisby Introduces Composable Functional JavaScript',
+          url: 'https://egghead.io/courses/professor-frisby-introduces-composable-functional-javascript',
+          page: 'Egghead'
+        }, {
+          id: 4,
+          name: 'Functional Programming Principles in Scala - École Polytechnique Fédérale de Lausanne',
+          url: 'https://www.coursera.org/learn/progfun1/home/welcome',
+          page: 'Coursera'
+        }, {
+          id: 5,
+          name: 'Functional Program Design in Scala - École Polytechnique Fédérale de Lausanne',
+          url: 'https://www.coursera.org/learn/progfun2/home/welcome',
+          page: 'Coursera'
+        }, {
+          id: 6,
+          name: 'Parallel programming - École Polytechnique Fédérale de Lausanne',
+          url: 'https://www.coursera.org/learn/parprog1/home/welcome',
+          page: 'Coursera'
+        }, {
+          id: 7,
+          name: 'Big Data Analysis with Scala and Spark - École Polytechnique Fédérale de Lausanne',
+          url: 'https://www.coursera.org/learn/scala-spark-big-data/home/welcome',
+          page: 'Coursera'
+        }, {
+          id: 8,
+          name: 'Machine Learning - Stanford University',
+          url: 'https://www.coursera.org/learn/machine-learning/home/welcome',
+          page: 'Coursera'
+        }, {
+          id: 9,
+          name: 'Data Science & Maschinelles Lernen in Python - am Beispiel',
+          url: 'https://www.udemy.com/data-science-und-machine-learning/',
+          page: 'Udemy'
+        }, {
+          id: 10,
+          name: 'Data Science A-Z™: Real-Life Data Science Exercises Included',
+          url: 'https://www.udemy.com/datascience/',
+          page: 'Udemy'
+        }, {
+          id: 11,
+          name: 'Neural Networks - 3Blue1Browm',
+          url: 'https://www.youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi',
+          page: 'Youtube'
         }
       ]
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
