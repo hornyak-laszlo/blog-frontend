747e80a36550754d701442b02a457f3560d478d7
Laszlo Hornyak <laszlo.hornyak@botfriends.de>
2019-05-27 19:16:25 +0200

    Packages updated

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
 
@@ -9,10 +9,7 @@ Vue.config.productionTip = false
 
 Vue.use(Buefy)
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
