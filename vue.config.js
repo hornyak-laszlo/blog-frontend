5f04c2f7b0db1ea6407f1145953a5c11d116b5c9
Laszlo Hornyak <laszlo.hornyak@botfriends.de>
2019-07-01 14:48:41 +0200

    npm package upd, seo

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: [
+          '/',
+          '/about',
+          '/articles',
+          '/courses',
+          '/contact'
+        ]
+      })
+    ]
+  }
+}
